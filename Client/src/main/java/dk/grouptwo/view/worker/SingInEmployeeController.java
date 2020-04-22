85fb750400255c121b5427cfd9537e2fa7955745
glad1o <2000Rulet?isabadoass>
2020-04-22 16:38:31 +0200

    fxml controller

---
+++ Client/src/main/java/dk/grouptwo/view/worker/SingInEmployeeController.java
@@ -0,0 +1,29 @@
+package dk.grouptwo.view.worker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SingInEmployeeController {
+
+    @FXML
+    private TextField signInEmployerCPRTextField;
+
+    @FXML
+    private TextField signInEmployeePasswordTextField;
+
+    @FXML
+    private Label signInEmployeeErrorLabel;
+
+    @FXML
+    void backEmployeeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logInEmployeeButtonPressed(ActionEvent event) {
+
+    }
+
+}
