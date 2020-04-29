a2ff62626605495dc42a4b026da3c051adde5cc0
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2020-04-29 16:17:01 +0200

    rmi clientside

--- Client/src/main/java/dk/grouptwo/networking/remote/RemoteClient.java
+++ Client/src/main/java/dk/grouptwo/networking/remote/RemoteClient.java
@@ -1,4 +1,37 @@
 package dk.grouptwo.networking.remote;
 
-public interface RemoteClient {
+import dk.grouptwo.model.objects.Employer;
+import dk.grouptwo.model.objects.Job;
+import dk.grouptwo.model.objects.Worker;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface RemoteClient extends Remote
+
+{
+  //Add remove jobs
+  void addJob(Job job) throws RemoteException;
+  void removeJob(Job job) throws RemoteException;
+
+  //Logins  **** If null don't load anything
+  Employer loginEmployer(String CVR, String password) throws RemoteException;
+  Worker loginWorker(String CPR, String password) throws RemoteException;
+
+  //Creating accounts **** these methods should be available only if the passwords match in the gui and the critical fields are not empty
+  void createEmployerAccount(Employer employer, String password) throws RemoteException;
+  void createWorkerAccount(Worker worker, String password) throws RemoteException;
+
+  //This should load every time the program starts, but then listeners will update gui when jobs are being added or removed
+  ArrayList<Job> getAllJobs() throws RemoteException;
+
+  // gucci ?
+  ArrayList<Job> getAllJobHistoryWorker() throws RemoteException;
+  ArrayList<Job> getAllJobHistoryEmployer() throws RemoteException;
+  ArrayList<Job> getUpcomingJobsWorker() throws RemoteException;
+
+  //Edit profiles these methods should be available only if the passwords match in the gui and the critical fields are not empty
+  void editEmployer(Employer employer, String password) throws RemoteException;
+  void editWorker(Worker worker, String password) throws RemoteException;
 }
--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module dk.grouptwo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    //requires com.google.gson;
+  requires java.rmi;
+  //requires com.google.gson;
 
     opens dk.grouptwo to javafx.fxml;
     opens dk.grouptwo.view to javafx.fxml;
