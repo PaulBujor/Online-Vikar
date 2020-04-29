9214348d32b6c2b0d03bd936c5d53c16aa497fe7
glad1o <2000Rulet?isabadoass>
2020-04-29 16:15:30 +0200

    model

--- Client/src/main/java/dk/grouptwo/model/EmployerModel.java
+++ Client/src/main/java/dk/grouptwo/model/EmployerModel.java
@@ -1,4 +1,15 @@
 package dk.grouptwo.model;
 
+import dk.grouptwo.model.objects.Job;
+
+
 public interface EmployerModel {
+
+    public void createWorkOffer(Job job);
+
+    public void cancelWorkOffer(Job job);
+
+    public void updateWorkOffer(Job job);
+
+
 }
--- Client/src/main/java/dk/grouptwo/model/WorkerModel.java
+++ Client/src/main/java/dk/grouptwo/model/WorkerModel.java
@@ -1,4 +1,18 @@
 package dk.grouptwo.model;
 
+import dk.grouptwo.model.objects.License;
+import dk.grouptwo.model.objects.Worker;
+
+
+import java.util.ArrayList;
+
 public interface WorkerModel {
+
+    public void applyForJob(Worker worker);
+
+    public void addLicense(License license);
+
+    public void deleteLicense(License license);
+
+
 }
