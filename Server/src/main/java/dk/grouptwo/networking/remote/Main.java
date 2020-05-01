6cfd0d32d66de6c182048fb80e963ef2ea58f571
fanimator12 <torok.franciska.leonora@gmail.com>
2020-05-01 16:56:49 +0200

    d

---
+++ Server/src/main/java/dk/grouptwo/networking/remote/Main.java
@@ -0,0 +1,25 @@
+package main.java.dk.grouptwo.networking.remote;
+
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.ExportException;
+
+public class Main {
+    public static void startRegistry() throws RemoteException {
+        try{
+            Registry reg = LocateRegistry.createRegistry(1099);
+            System.out.println("Registry started...");
+        } catch (ExportException e) {
+            System.out.println("Registry already started?\n" + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
+        startRegistry();
+        Server server = new Server();
+        server.start();
+        System.out.println("Server started...");
+    }
+}
