1b21e473803309ed460767ad26686993f9354f40
fanimator12 <torok.franciska.leonora@gmail.com>
2020-05-03 14:58:19 +0200

    Update Main.java

--- Server/src/main/java/dk/grouptwo/networking/Main.java
+++ Server/src/main/java/dk/grouptwo/networking/Main.java
@@ -11,12 +11,23 @@ import java.rmi.server.ExportException;
 
 
 public class Main {
-
+ /*   public static void startRegistry() throws RemoteException {
+        try{
+            Registry reg = LocateRegistry.createRegistry(1099);
+            System.out.println("Registry started...");
+        } catch (ExportException e) {
+            System.out.println("Registry already started?\n" + e.getMessage());
+        }
+    }
+*/
     public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
-        RemoteServer server = new Server();
-        Registry registry = LocateRegistry.createRegistry(1099);
-
-        registry.bind("Server",server);
-        System.out.println("Server started..");
+        try{
+            RemoteServer server = new Server();
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.bind("Server",server);
+            System.out.println("Server started..");
+        } catch (ExportException e) {
+            System.out.println("Server already started?\n" + e.getMessage());
+        }
     }
 }
