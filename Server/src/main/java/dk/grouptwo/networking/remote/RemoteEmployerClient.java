87a8e09d99fc1174e47e2d0bf698f8027c39ed06
Paul <bujor.paul2000@gmail.com>
2020-05-18 17:02:22 +0200

    fix remote ints

--- Server/src/main/java/dk/grouptwo/networking/remote/RemoteEmployerClient.java
+++ Server/src/main/java/dk/grouptwo/networking/remote/RemoteEmployerClient.java
@@ -2,8 +2,9 @@ package dk.grouptwo.networking.remote;
 
 import dk.grouptwo.model.objects.Job;
 
+import java.io.Serializable;
 import java.rmi.Remote;
 
-public interface RemoteEmployerClient extends Remote {
+public interface RemoteEmployerClient extends Remote, Serializable {
     public void updateJob(Job job);
 }
