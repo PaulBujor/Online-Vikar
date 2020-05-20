4681de60a812e81a2bd61e7ac39cb5d014cb0320
fanimator12 <torok.franciska.leonora@gmail.com>
2020-05-20 17:22:03 +0200

    fix, uml

--- Client/src/main/java/dk/grouptwo/view/SignInController.java
+++ Client/src/main/java/dk/grouptwo/view/SignInController.java
@@ -20,7 +20,7 @@ public class SignInController {
     }
 
     @FXML
-    public void singInEmployerButtonPressed() {
+    public void signInEmployerButtonPressed() {
         viewHandler.openView("signInEmployer");
     }
 
